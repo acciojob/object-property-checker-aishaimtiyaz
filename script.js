896b22a71a2240575e00645894dcf1d575507a0e
aishaimtiyaz <144771688+aishaimtiyaz@users.noreply.github.com>
2024-02-23 11:25:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,11 +2,14 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
-	if(sampleObject[key] !== undefined)
-		return true;
-	else{
+	for(let i in sampleObject)
+		{
+			if(i === key)
+		    return true;
+		}
+	
 		return false;
-	}
+	
 }
 
 // Do not change the code below
